e0a30e6b1b97fbe243f1fc8aa88d367ee4d4a0d8
Michael <96860840+SpiredFormula@users.noreply.github.com>
2025-12-17 21:17:22 +1100

    Move datagen providers to client directory

--- src/client/java/spired/spiredsextramaterials/datagen/SpiredsExtraMaterialsModelProvider.java
+++ src/client/java/spired/spiredsextramaterials/datagen/SpiredsExtraMaterialsModelProvider.java
@@ -1,5 +1,6 @@
 package spired.spiredsextramaterials.datagen;
 
+
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
